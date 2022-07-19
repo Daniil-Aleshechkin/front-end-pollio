9dd045e6161237760628d92b3b9b0fc61cc68a1a
Daniil Aleschechkin <DaniilA@iqmetrix.com>
2022-07-18 19:00:02 -0600

    removed unused reference

--- pages/main-page/index.js
+++ pages/main-page/index.js
@@ -2,7 +2,6 @@ require("normalize.css");
 require("../../public/common.css");
 require("../../public/navResponsive.css");
 require("./mainPage.css");
-require("./graphInputs.css");
 require("./graph.css");
 require("./login-page.css");
 require("./responsive.css");
