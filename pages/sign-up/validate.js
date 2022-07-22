4f6a99c8938c17b67ab64655422a6ddb55b19896
Daniil-Aleshechkin <aleshechkin.daniil@gmail.com>
2022-07-22 11:32:24 -0600

    Added validation in signup

---
+++ pages/sign-up/validate.js
@@ -0,0 +1,90 @@
+const getUserFriendlyName = {
+    "email" : "Email",
+    "password": "Password",
+    "confirmPassword": "Confirm password",
+    "username": "Username"
+}
+
+document.getElementById("submit-button")[0]
+
+function validateForm(form) {
+    errors = getValidationErrors(form)
+
+    if (errors.length != 0) {
+        clear_errors()
+        displayErrors(errors);
+        return false
+    } else {
+        return true
+    }
+}
+
+function onFormSubmit(e) {
+    let signUpForm = document.getElementById("sign-up-from")
+    e.preventDefault()
+    if (validateForm(signUpForm)) {
+
+    }
+}
+
+function clear_errors() {
+    Array.from(document.getElementsByClassName("error-container")).forEach(errorContainer => errorContainer.replaceChildren())
+}
+
+function displayErrors(errors) {
+    errors.forEach(error => {
+        let errorContainer = document.getElementById(`${error.errorSource}-error`)
+
+        let errorHeading = document.createElement("div");
+        errorHeading.className= "error-container";
+        let errorText = document.createTextNode(error.errorMessage);
+        errorHeading.appendChild(errorText);
+
+        errorContainer.appendChild(errorHeading);
+    })
+}
+
+function getValidationErrors(form) {
+    let errors = [];
+    let inputs = form.getElementsByTagName("input");
+    
+    // All input fields must be filled out
+    let unfilledFields = [];
+
+    Array.from(inputs).forEach(input => {
+        if (input.textLength <= 0){
+            unfilledFields.push(getUserFriendlyFieldName[input.name])
+            errors.push({errorMessage: getUserFriendlyFieldName[input.name]+" is required", errorSource: input.name})
+        }
+    });
+    
+    // Email must be less than 60 characters
+    if (inputs["email"].textLength > 60 && inputs["email"].textLength !== 0)
+        errors.push({errorMessage: "Maximum length of \"Email\" is 60 characters.", errorSource:"email"});
+
+    // Email must be a valid email address
+    if (!inputs["email"].value.match(/^[\w\.]+@[\w\.]+[\w]/) && inputs["email"].textLength !== 0)
+        errors.push({errorMessage: "Please enter a valid email", errorSource: "email"})
+
+    // Username must only contain alpha numeric characters
+    if (inputs["username"].value.match(/[^0-9A-Za-z]/) && inputs["username"].textLength !== 0)
+        errors.push({errorMessage: "Username must only contain alphanumeric characters. No special symbols or spaces", errorSource: "username"});
+
+    // Maximum length of username is 40 characters
+    if (inputs["username"].textLength > 40 && inputs["username"].textLength !== 0)
+        errors.push({errorMessage: "Maximum length of \"Username\" is 40 characters.", errorSource:"username"});
+
+    // Password must contain letters and a digit
+    if (inputs["password"].value.match(/^([a-zA-Z0-9]+)$/) && inputs["password"].value.match(/\d/) && inputs["password"].textLength !== 0)
+        errors.push({errorMessage:"Password must contain at least one number and letters", errorSource:"password"});
+    
+    // Password must be longer than 12 characters
+    if (inputs["password"].textLength > 12 && inputs["password"].textLength !== 0)
+        errors.push({errorMessage:"\"Password\" length must be exactly 8 characters.", errorSource:"password"});
+
+    // Confirm password and password must match
+    if (inputs["password"].value != inputs["confirmPassword"].value && inputs["password"].textLength !== 0 && inputs["confirmPassword"].textLength !== 0)
+        errors.push({errorMessage:"\"Confirm Password\" field and \"Password\" field must match.", errorSource:"confirmPassword"});
+
+    return errors;
+}
