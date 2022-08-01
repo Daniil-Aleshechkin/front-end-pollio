3afffaad07e82859d5c96b7d58485b6cd456fb1d
Daniil-Aleshechkin <aleshechkin.daniil@gmail.com>
2022-08-01 00:45:10 -0700

    Added reponsive calander widget

--- pages/poll-creation/index.js
+++ pages/poll-creation/index.js
@@ -3,6 +3,7 @@ require("../../public/common.css");
 require("../../public/navResponsive.css");
 require("./poll.css");
 require("./calander.css")
+require("./responsive.css")
 
 require("./poll.js")
 require("./validate.js")
